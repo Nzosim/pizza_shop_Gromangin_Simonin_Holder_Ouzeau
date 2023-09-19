69753c0cc2a30ca32d2bc36de1da7c803e4a7461
Nzosim <nzosim54@gmail.com>
2023-09-19 14:19:50 +0200

    jsplus

---
+++ shop.pizza-shop/src/domain/entities/shop/Commande.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace pizzashop\shop\domain\entities\shop;
+
+class Commande extends \Illuminate\Database\Eloquent\Model
+{
+
+}
--- shop.pizza-shop/src/domain/service/commande/commande.php
+++ shop.pizza-shop/src/domain/service/commande/commande.php
@@ -7,6 +7,7 @@ use pizzashop\shop\domain\dto\catalogue\CommandeDTO;
 interface commande {
 
     function accederCommande(string $UUID) : CommandeDTO;
-    function validerCommande(string $UUID) : void;
+    function validerCommande(string $UUID) : CommandeDTO;
+    function creerCommande(CommandeDTO $commandeDTO) : CommandeDTO;
 
 }

1bd8e1a84da60d120d7cb852994afc77a1fb3f59
Jules <jules.holder.54@gmail.com>
2023-10-10 15:13:22 +0200

    oui

--- shop.pizza-shop/config/bootstrap.php
+++ shop.pizza-shop/config/bootstrap.php
@@ -3,27 +3,30 @@
 namespace pizzashop\config;
 
 use DI\ContainerBuilder;
-use Psr\Container\ContainerInterface;
-use Slim\Factory\AppFactory;
 use Illuminate\Database\Capsule\Manager as Eloquent;
+use Slim\Factory\AppFactory;
 
 
-$settings=require_once __DIR__.'/settings.php';
-$dependencies=require_once __DIR__.'/services_dependencies.php';
-$actions=require_once __DIR__.'/actions_dependencies.php';
+$settings = require_once __DIR__ . '/settings.php';
+$dependencies = require_once __DIR__ . '/services_dependencies.php';
+$actions = require_once __DIR__ . '/actions_dependencies.php';
 
-$builder=new ContainerBuilder();
-$builder->addDefinitions($settings);
-$builder->addDefinitions($dependencies);
-$builder->addDefinitions($actions);
-$c=$builder->build();
-$app= AppFactory::createFromContainer($c);
+$builder = new ContainerBuilder();
+//$builder->addDefinitions($settings);
+//$builder->addDefinitions($dependencies);
+//$builder->addDefinitions($actions);
+$c = $builder->build();
+$app = AppFactory::createFromContainer($c);
 
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
-$app->addErrorMiddleware($c->get('displayErrorDetails'), false,false)->getDefaultErrorHandler()->forceContentType('application/json');
+//$app->addErrorMiddleware($c->get('displayErrorDetails'), false, false)->getDefaultErrorHandler()->forceContentType('application/json');
+$app->addErrorMiddleware(true, false, false);
 
-Eloquent::create()->addConnection(parse_ini_file(__DIR__.'/../commande.db.ini','commande'))->addConnection(parse_ini_file(__DIR__.'/../catalog.db.ini','catalogue'));
+
+Eloquent::create()
+    ->addConnection(parse_ini_file(__DIR__ . '/commande.db.ini', 'commande'))
+    ->addConnection(parse_ini_file(__DIR__ . '/catalog.db.ini', 'catalogue'));
 
 
 return $app;

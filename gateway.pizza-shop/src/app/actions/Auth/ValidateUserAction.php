a488dc16eab31fc8d601aebce302ecdff51a7a8c
clemgrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-01-06 18:01:59 +0100

    gestions des erreurs de token

--- gateway.pizza-shop/src/app/actions/Auth/ValidateUserAction.php
+++ gateway.pizza-shop/src/app/actions/Auth/ValidateUserAction.php
@@ -9,47 +9,59 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 /**
- * action qui permet de valider un token
+ * Action qui permet de valider un token
  */
 class ValidateUserAction
 {
 
     public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
     {
-
         try {
+           
             $authorizationHeader = $rq->getHeaderLine('Authorization');
 
+            
             $data = GuzzleRequest::MakeRequest('GET', 'auth', "users/validate", false, $authorizationHeader);
-            $code = 200;
+            $code = 200; 
         } catch (\Exception $e) {
-            if($e->getCode() == 401) {
+   
+            if ($e instanceof GuzzleException) {
+                // On récupére la réponse associée à l'exception s'il y en a une
+                $response = $e->hasResponse() ? $e->getResponse() : null;
 
-                $code = 401;
-                $data = [
-                    "message" => "401 Unauthorized",
-                    "exception" => [[
-                        "type" => "Slim\\Exception\\HttpUnauthorizedException",
-                        "message" => "Token invalide ou expiré",
-                        "code" => $e->getCode(),
-                        "file" => $e->getFile(),
-                        "line" => $e->getLine(),
-                    ]]
-                ];
-            }else {
+                if ($response !== null) {
+                    // On récupére le code de statut de la réponse et le message JSON
+                    $statusCode = $response->getStatusCode();
+                    $message = json_decode($response->getBody(), true);
+
+                    // on gére le cas où le code de statut est 401 et qu'il y a une clé 'exception' dans le message
+                    if ($statusCode === 401 && isset($message['exception'])) {
+                        $code = 401; 
+                        $data = $message; 
+                    } else {
+                        // on utilise le code de statut et le message d'erreur par défaut
+                        $code = $statusCode;
+                        $data = [
+                            "error" => $e->getMessage(),
+                            "code" => $e->getCode()
+                        ];
+                    }
+                }
+            } else {
+                // On gérer les autres exceptions 
+                $code = 500; 
                 $data = [
                     "error" => $e->getMessage(),
                     "code" => $e->getCode()
                 ];
-                $code = 500;
             }
         }
-        // on retourne la réponse avec le code et les données
+
+         // on retourne la réponse avec le code et les données
         return JSONRenderer::render($rs, $code, $data)
             ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Methods', 'GET' )
+            ->withHeader('Access-Control-Allow-Methods', 'GET')
             ->withHeader('Access-Control-Allow-Credentials', 'true')
             ->withHeader('Content-Type', 'application/json');
     }
 }
-

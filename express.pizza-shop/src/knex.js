be171c4e28dbffc6d0e626e790d12e555c42b5eb
Nzosim <nzosim54@gmail.com>
2024-01-23 15:44:24 +0100

    nickel

--- express.pizza-shop/src/knex.js
+++ express.pizza-shop/src/knex.js
@@ -1,14 +1,13 @@
 import knex from "knex";
 
-const knex = knex({
+const bdd = knex({
     client: "mysql",
     connection: {
         host: "pizza-shop.commande.db",
-        port: 41219,
+        port: 3306,
         user: "pizza_shop",
         password: "pizza_shop",
         database: "pizza_shop"
     }
 });
-
 

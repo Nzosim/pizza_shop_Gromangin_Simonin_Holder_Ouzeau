56cfde3bd2870ef14469d4ecf540a950c6a6f79b
Nzosim <nzosim54@gmail.com>
2024-01-30 16:34:11 +0100

    add

--- websockets.pizza-shop/src/server.js
+++ websockets.pizza-shop/src/server.js
@@ -1,4 +1,5 @@
 import WebSocket, { WebSocketServer } from 'ws';
+import amqp from 'amqplib';
 
 const server = new WebSocketServer({ port: 3000 , clientTracking: true});
 
@@ -8,13 +9,57 @@ const notifyAll = (msg) =>{
             client_socket.send(msg);
     });
 }
+
+const clients = [];
+
 server.on('connection', (client_socket) => {
     client_socket.on('error', console.error);
     client_socket.on('message', (message) => {
         console.log('received: %s', message);
+        clients.push([client_socket, message]);
         client_socket.send('Vous êtes abonné à cette commande : ' + message);
     });
     client_socket.on('close', (event) => {
+        clients.splice(clients.indexOf(client_socket), 1);
         console.log('client disconnected ');
     });
 });
+
+
+
+
+class serviceConsommation {
+
+    rabbitmq = "";
+    queue = "";
+    exchange = "";
+    routingKey = "";
+    connect = null;
+    channel = null;
+
+    constructor(rabbitmq, queue, exchange, routingKey) {
+        this.rabbitmq = rabbitmq;
+        this.queue = queue;
+        this.exchange = exchange;
+        this.routingKey = routingKey;
+    }
+
+    async consume() {
+        this.connect = await amqp.connect(this.rabbitmq);
+        this.channel = await this.connect.createChannel();
+        this.channel.consume(this.queue, (msg) => {
+            let message = msg.content.toString()
+            message = JSON.parse(message);
+            let client = clients.find((client) => (client[1]).toString('utf8') === message.id);
+            console.log("client : " + client);
+            if (client) {
+                client[0].send('Votre commande vient de passer à l\'étape : ' + message.etape );
+            }
+            this.channel.ack(msg);
+        })
+    }
+
+}
+
+let suivi = new serviceConsommation('amqp://user:user@rabbitmq', 'suivi_commandes', 'pizzashop', 'suivi');
+suivi.consume();

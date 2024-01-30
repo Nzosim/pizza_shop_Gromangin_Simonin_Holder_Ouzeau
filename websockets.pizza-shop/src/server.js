fcad0c917fd02c0adba289b930f9823e2e11744b
Nzosim <nzosim54@gmail.com>
2024-01-30 15:08:04 +0100

    update

---
+++ websockets.pizza-shop/src/server.js
@@ -0,0 +1,21 @@
+import WebSocket, { WebSocketServer } from 'ws';
+
+const server = new WebSocketServer({ port: 3000 , clientTracking: true});
+
+const notifyAll = (msg) =>{
+    server.clients.forEach((client_socket) => {
+        if (client_socket.readyState === WebSocket.OPEN)
+            client_socket.send(msg);
+    });
+}
+server.on('connection', (client_socket) => {
+    client_socket.on('error', console.error);
+    client_socket.on('message', (message) => {
+        console.log('received: %s', message);
+        client_socket.send('acknowledge msg : ' + message);
+        notifyAll(message);
+    });
+    client_socket.on('close', (event) => {
+        console.log('client disconnected ');
+    });
+});
